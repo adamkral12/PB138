531648b13d1f6e303bf8b5002c39774a2ffcc3a5
adamkral12 <xkral3@nymfe01.fi.muni.cz>
2017-05-04 19:01:44 +0200

    Build class Country

--- PhoneCallManager/src/core/Country.java
+++ PhoneCallManager/src/core/Country.java
@@ -9,6 +9,34 @@ package core;
  *
  * @author xkral3, xvalko, xmikova
  */
-public class Country {
+final public class Country {
+    private final int id;
+    private final String name;
+    private final Region region;
+    private final String prefix;
+
+    public Country(int id, String name, Region region, String prefix) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+        this.prefix = prefix;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public final Region getRegion() {
+        return region;
+    }
+
+    public final String getPrefix() {
+        return prefix;
+    }
+    
     
 }
